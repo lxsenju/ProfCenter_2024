fca410c2fedff9db0e96c848128a9d12c869a442
lxsenju <121480493+lxsenju@users.noreply.github.com>
2024-06-14 23:31:28 +0700

    feat: set order status logic

--- admin.php
+++ admin.php
@@ -10,6 +10,7 @@ include "./php/db.php"
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/index.css" type="text/css">
     <link rel="stylesheet" href="style/header-footer.css" type="text/css">
+    <link rel="stylesheet" href="style/addition.css" type="text/css">
     <script src="js/index.js" defer></script>
     <title>Админ</title>
 </head>
@@ -21,10 +22,11 @@ require_once "header.php"
     <h1>Заказы</h1>
     <section class="section section--form">
     <?php
-    $sql = "SELECT CONCAT(user.fname, ' ', user.lname) AS name, service_type.name AS service_type_name, service.name AS service_name, ordering.email
+    $sql = "SELECT CONCAT(user.fname, ' ', user.lname) AS name, service_type.name AS service_type_name, service.name AS service_name, ordering_status.name AS ordering_status_name, ordering_status.ID_ordering_status AS ID_ordering_status, ordering.email, ordering.ID_ordering AS ID_ordering
         FROM ordering
         JOIN user ON ordering.ID_user = user.ID_user 
         JOIN service ON ordering.ID_service = service.ID_service 
+        JOIN ordering_status ON ordering.ID_ordering_status = ordering_status.ID_ordering_status
         JOIN service_type ON service.ID_service_type = service_type.ID_service_type
         ORDER BY ordering.ID_ordering_status;";
     $result = $connection->query($sql);
@@ -46,8 +48,20 @@ require_once "header.php"
                 echo '</div>';
             echo '</div>';
             echo '<div class="orders__col">';
-                echo '<form><button type="submit">Одобрить</button></form>';
-                echo '<form><button type="submit">Отклонить</button></form>';
+            if ($n['ID_ordering_status'] == '1') {
+                echo "<form method='post' action='php/set_status-order.php'>
+                      <label class='none'><input name='id_order' type='text' value='" . $n['ID_ordering'] . "'></label>
+                        <button name='order_status' value='2' type='submit'>Одобрить</button>
+                      </form>
+
+                      <form method='post' action='php/set_status-order.php'>  
+                       <label class='none'><input name='id_order' type='text' value='" . $n['ID_ordering'] . "'></label>
+                        <button name='order_status' value='3' type='submit'>Отклонить</button>
+                      </form>";
+            }
+            else {
+                echo $n['ordering_status_name'];
+        }
             echo '</div>';
         echo '</div>';
     };
